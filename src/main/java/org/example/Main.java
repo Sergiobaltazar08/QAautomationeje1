0f2936dcc5d23964ce8b4da1aaf20b9751bf6426
Sergio Baltazar <baltazarsergiodavid@gmail.com>
2023-09-09 20:20:15 -0400

    added from Itellij

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int a=9;
+        int b=8;
+        if( a > b){
+            a=a*2;
+            System.out.println(a);
+        }else{
+            b=b-2;
+            System.out.println(b);
+        }
+        System.out.println("Hello world!");
+    }
+}
